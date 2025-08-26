f73d28afcb3510ce8b4115d8ad66c36abf8c785a
Masputrawae <masputrawae.official@gmail.com>
2025-08-26 22:20:13 +0700

    Changed On 📅 : 2025-08-26 22:20:13

--- assets/js/module_internal/sidebar.js
+++ assets/js/module_internal/sidebar.js
@@ -1,9 +1,26 @@
-import { $$, byId } from "../helper";
+import { $$, byId } from "js/helper";
 
 export const sidebarHandler = () => {
   $$("[data-sidebar]").forEach((btn) => {
-    btn.addEventListener("click", () => {
+    btn.addEventListener("click", (e) => {
+      e.stopPropagation();
       byId("sidebar").classList.toggle("sidebar--is-active");
+    });
+  });
+
+  document.addEventListener("click", (e) => {
+    if (
+      byId("sidebar").classList.contains("sidebar--is-active") &&
+      !byId("sidebar").contains(e.target) &&
+      !e.target.closest("[data-sidebar]")
+    ) {
+      byId("sidebar").classList.remove("sidebar--is-active");
+    }
+  });
+
+  $$("a, button", byId("sidebar")).forEach((el) => {
+    el.addEventListener("click", () => {
+      byId("sidebar").classList.remove("sidebar--is-active");
     });
   });
 };
